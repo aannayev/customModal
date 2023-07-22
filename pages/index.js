2c1192eb992c8b1ee0dd3690eab4e72491a3c9f8
interstels@gmail.com <interstels@gmail.com>
2023-07-22 15:40:52 +0500

    22.07 part 2

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,35 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import styles from "../styles/Home.module.css";
-
+import AppExtensionsSDK from "@pipedrive/app-extensions-sdk";
+import { useEffect } from "react";
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    async function openCustomFrontendModal() {
+      const identifier = "my_custom_ui_identifier"; // Manually set the identifier
+      const customUISize = { height: 500 }; // Manually set the custom UI size
+
+      const sdk = await new AppExtensionsSDK({ identifier }).initialize({
+        size: customUISize,
+      });
+
+      // Now you can use the `sdk` object to interact with Pipedrive and display your custom UI
+      // For example:
+      // sdk.modal.setTitle('Custom Frontend');
+      // sdk.modal.setContent('<iframe src="/custom_frontend" frameborder="0" style="width: 100%; height: 100%;"></iframe>');
+      // sdk.modal.setWidth('90%');
+      // sdk.modal.setHeight('90%');
+      // sdk.modal.setIsFullWidth(true);
+      // sdk.modal.setIsFullHeight(true);
+      // sdk.modal.setIsHiddenOnClose(false);
+      // sdk.modal.open();
+    }
+
+    openCustomFrontendModal();
+  }, [router]);
+
   return (
     <div className={styles.container}>
       <Head>
