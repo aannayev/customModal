d5c529ffc45badae4100427ef440896ed53c7b52
interstels@gmail.com <interstels@gmail.com>
2023-07-24 11:17:00 +0500

    fs

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,11 @@
 // pages/index.js
-
-import { useEffect } from "react";
-import CustomModal from "../components/CustomModal";
+import CustomComponent from "../components/CustomModal";
 
 const HomePage = () => {
-  useEffect(() => {
-    // Example of triggering the custom modal on page load
-    CustomModal.openCustomModal();
-  }, []);
-
   return (
     <div>
       <h1>Hello, Next.js App!</h1>
+      <CustomComponent />
     </div>
   );
 };
