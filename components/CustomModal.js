c66682739d04bdbc2166034f7de73f1e1ab56509
interstels@gmail.com <interstels@gmail.com>
2023-07-24 11:15:05 +0500

    fs

--- components/CustomModal.js
+++ components/CustomModal.js
@@ -1,36 +1,47 @@
-// components/CustomModal.js
+// components/CustomComponent.js
 
 import { useEffect } from "react";
 import { init } from "@pipedrive/app-extensions-sdk";
 
-const CustomModal = () => {
+const CustomComponent = () => {
   useEffect(() => {
-    // Function to create and open the custom modal
-    async function openCustomModal() {
+    async function initializeSDK() {
       try {
-        const { modal } = await init();
+        const queryParams = new URLSearchParams(window.location.search);
+        const identifier = queryParams.get("id");
 
+        if (!identifier) {
+          console.error("Identifier not found in URL query parameters.");
+          return;
+        }
+
+        // Use the identifier as needed
+        console.log("Identifier:", identifier);
+
+        // Optionally, you can set custom UI size if needed
+        const sdk = await new AppExtensionsSDK({
+          identifier: identifier,
+        }).initialize({
+          size: { height: 500 }, // Replace this with your desired custom UI size
+        });
+
+        // Use the sdk object to interact with Pipedrive, for example, opening a modal
+        const { modal } = sdk;
         modal.setTitle("Custom Modal");
         modal.setContent("<p>This is the content of the custom modal.</p>");
         modal.setWidth("500px");
         modal.setHeight("300px");
         modal.setIsHiddenOnClose(true);
-
         modal.open();
       } catch (error) {
         console.error("Error initializing the Pipedrive SDK:", error);
       }
     }
 
-    openCustomModal();
+    initializeSDK();
   }, []);
 
-  return null;
+  return null; // Since this component handles the modal initialization, it doesn't render anything itself
 };
 
-CustomModal.openCustomModal = () => {
-  // Method to trigger the custom modal
-  return <CustomModal />;
-};
-
-export default CustomModal;
+export default CustomComponent;
